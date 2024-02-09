42c5e952eba7fc7c3f0836d641c3c3d263b27a34
Oguzhan Ulutas <mkoulutas@gmail.com>
2024-02-09 20:05:18 +0300

    Add Footer

---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+
+
+const Footer:FC = () => {
+  return (
+    <footer className="footer">
+      2024 - Coded by{" "}
+      <a href="https://github.com/oguzhan-ulutas">oguzhan-ulutas</a>
+    </footer>
+  );
+};
+
+export default Footer;
--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,11 +1,13 @@
 
 import React from 'react'
 import Header from './Header'
+import Footer from './Footer'
 
 export const Home:React.FC = () => {
   return (
     <div className='home-container' >
         <Header/>
+        <Footer/>
     </div>
   )
 }

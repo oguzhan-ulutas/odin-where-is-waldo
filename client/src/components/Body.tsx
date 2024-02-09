bba898e75867dfe65fed29cbab2a9c9532f44327
Oguzhan Ulutas <mkoulutas@gmail.com>
2024-02-09 20:10:38 +0300

    Add Body

---
+++ client/src/components/Body.tsx
@@ -0,0 +1,14 @@
+import { FC } from "react";
+
+import baldies from "../assets/baldies.png"
+
+const Body:FC = ()=>{
+
+    return(
+        <>
+        <img src={baldies} alt="baldies" />
+        </>
+    )
+}
+
+export default Body
--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -2,11 +2,13 @@
 import React from 'react'
 import Header from './Header'
 import Footer from './Footer'
+import Body from './Body'
 
 export const Home:React.FC = () => {
   return (
     <div className='home-container' >
         <Header/>
+        <Body/>
         <Footer/>
     </div>
   )
